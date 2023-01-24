8d5b51171bbb5bf73b2dcf20b48f40b3f28b3b87
amird55 <amir.dgn@outlook.com>
2023-01-24 10:12:15 +0200

    client ready

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express'); //Import the express dependency
+const port = 4444;                  //Save the port number where your server will be listening
+const app = express();//Instantiate an express app, the main work horse of this server
+app.set("view engine", "ejs");
+var cnt=0;
+
+app.use('/CSS', express.static(__dirname + "/CSS"));
+
+let lastMove={"player":1,"cell":5};
+router.get('/GetLast',function(req,res){
+  res.send(lastMove);
+});
+
+ const router = express.Router();
+router.get('/', (req, res) => {        //get requests to the root ("/") will route here
+      cnt++;
+      res.render("index", {
+      timesShown: cnt,
+    });
+                                                  //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
+
+
+//add the router
+app.use('/', router);
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`); 
+});
+
